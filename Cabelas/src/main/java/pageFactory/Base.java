956b19ca7fdce13a4a80e1c669320b233f783ba4
jcrd1994 <31782411+jcrd1994@users.noreply.github.com>
2020-12-10 00:23:00 -0600

    Configuracion de paquete pageFactory

--- Cabelas/src/main/java/pageFactory/Base.java
+++ Cabelas/src/main/java/pageFactory/Base.java
@@ -1,10 +1,13 @@
 package pageFactory;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+
 
 public class Base {
 	protected WebDriver driver;
@@ -32,7 +35,33 @@ public class Base {
 		return driver;
 	}
 
+	public void irSitioWeb(String url) {
+		driver.get(url);
+	}
 	
+	public WebElement findElement(By locator) {
+		return driver.findElement(locator);
+	}
+	
+	public List<WebElement> findElements(By locator) {
+		return driver.findElements(locator);
+	}
+	
+	public String getText(WebElement element) {
+		return element.getText();
+	}
+	
+	public String getText(By locator) {
+		return driver.findElement(locator).getText();
+	}
+	
+	public void escribe(String inputText, By locator) {
+		driver.findElement(locator).sendKeys(inputText);
+	}
+
+	public void click(By locator) {
+		driver.findElement(locator).click();
+	}
 
 
 }
